682189cc8b5eb26c59b1a45dcd9702a4f76da06c
HayatoKanbe <50877609+KANBE8810@users.noreply.github.com>
2020-12-10 13:03:30 +0900

    Update gaming.c

--- myled/gaming.c
+++ myled/gaming.c
@@ -24,107 +24,107 @@ static int gpio[4]={16,23,25,26};
 int i,cnt;
 
 static ssize_t gaming_write(struct file* filp, const char* buf, size_t count, loff_t* pos) {
-		char c;
-		if(copy_from_user(&c,buf,sizeof(char)))
-				return -EFAULT;
-
-		if(c == 'f' || C =='F') {	//Only the electric fan works
-				gpio_base[7] = 1 << 25;
-		} else if(c == 'G') {	//Similar movement on a gaming PC 1
-				gpio_base[7] = 1 << 25;
-				cnt=0;
-				while(cnt < 100){
-						gpio_base[7] = 1 << 23;
-						gpio_base[10] = 1 << 16;
-						gpio_base[7] = 1 << 26;
-						msleep(300);
-						gpio_base[10] = 1 << 23;
-						gpio_base[7] = 1 << 16;
-						gpio_base[10] = 1 << 26;
-						msleep(300);
-						cnt++;
-				}
-				gpio_base[10] = 1 << 23;
-				gpio_base[10] = 1 << 16;
-				gpio_base[10] = 1 << 26;
-		} else if(c == 'g') {	//Similar movement on a gaming PC 2
-				gpio_base[7] = 1 << 25;
-				cnt=0;
-				while(cnt < 100){
-						gpio_base[7] = 1 << 23;
-						gpio_base[10] = 1 << 16;
-						gpio_base[10] = 1 << 26;
-						msleep(200);
-						gpio_base[10] = 1 << 23;
-						gpio_base[7] = 1 << 16;
-						gpio_base[10] = 1 << 26;
-						msleep(200);
-						gpio_base[10] = 1 << 23;
-						gpio_base[10] = 1 << 16;
-						gpio_base[7] = 1 << 26;
-						msleep(200);
-						cnt++;
-				}
-				gpio_base[10] = 1 << 23;
-				gpio_base[10] = 1 << 16;
-				gpio_base[10] = 1 << 26;
+	char c;
+	if(copy_from_user(&c,buf,sizeof(char)))
+		return -EFAULT;
+	
+	if(c == 'f' || C =='F') {	//Only the electric fan works
+		gpio_base[7] = 1 << 25;
+	} else if(c == 'G') {	//Similar movement on a gaming PC 1
+		gpio_base[7] = 1 << 25;
+		cnt=0;
+		while(cnt < 100){
+			gpio_base[7] = 1 << 23;
+			gpio_base[10] = 1 << 16;
+			gpio_base[7] = 1 << 26;
+			msleep(300);
+			gpio_base[10] = 1 << 23;
+			gpio_base[7] = 1 << 16;
+			gpio_base[10] = 1 << 26;
+			msleep(300);
+			cnt++;
+		}
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 16;
+		gpio_base[10] = 1 << 26;
+	} else if(c == 'g') {	//Similar movement on a gaming PC 2
+		gpio_base[7] = 1 << 25;
+		cnt=0;
+		while(cnt < 100){
+			gpio_base[7] = 1 << 23;
+			gpio_base[10] = 1 << 16;
+			gpio_base[10] = 1 << 26;
+			msleep(200);
+			gpio_base[10] = 1 << 23;
+			gpio_base[7] = 1 << 16;
+			gpio_base[10] = 1 << 26;
+			msleep(200);
+			gpio_base[10] = 1 << 23;
+			gpio_base[10] = 1 << 16;
+			gpio_base[7] = 1 << 26;
+			msleep(200);
+			cnt++;
+		}
+			gpio_base[10] = 1 << 23;
+			gpio_base[10] = 1 << 16;
+			gpio_base[10] = 1 << 26;
 		} else if(c == 'E' || c == 'e') {	//All move stop
-				gpio_base[10] = 1 << 25;
-				gpio_base[10] = 1 << 23;
-				gpio_base[10] = 1 << 16;
-				gpio_base[10] = 1 << 26;
+			gpio_base[10] = 1 << 25;
+			gpio_base[10] = 1 << 23;
+			gpio_base[10] = 1 << 16;
+			gpio_base[10] = 1 << 26;
 		}
 		return 1;
 }
 
 static struct file_operations led_fops = {
-		.owner = THIS_MODULE,
-		.write = gaming_write
+	.owner = THIS_MODULE,
+	.write = gaming_write
 };
 
 static int __init init_mod(void) {
-		int retval;
+	int retval;
 
-		gpio_base = ioremap_nocache(0xfe200000, 0xA0); 
-		for(i=0;i<4;++i) {
-				const u32 led = gpio[i];
-				const u32 index = led/10;
-				const u32 shift = (led%10)*3;
-				const u32 mask = ~(0x7 << shift);
-				gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
-		}
-		retval =  alloc_chrdev_region(&dev, 0, 1, "gaming");
-		if(retval < 0) {
-				printk(KERN_ERR "alloc_chrdev_region failed.\n");
-				return retval;
-		}
-		printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+	gpio_base = ioremap_nocache(0xfe200000, 0xA0); 
+	for(i=0;i<4;++i) {
+		const u32 led = gpio[i];
+		const u32 index = led/10;
+		const u32 shift = (led%10)*3;
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	}
+	retval =  alloc_chrdev_region(&dev, 0, 1, "gaming");
+	if(retval < 0) {
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
+	}
+	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-		cdev_init(&cdv, &led_fops);
-		cdv.owner = THIS_MODULE;
-		retval = cdev_add(&cdv, dev, 1);
-		if(retval < 0) {
-				printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
-				return retval;
-		}
+	cdev_init(&cdv, &led_fops);
+	cdv.owner = THIS_MODULE;
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0) {
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+		return retval;
+	}
 
-		cls = class_create(THIS_MODULE,"gaming");
-		if(IS_ERR(cls)) {
-				printk(KERN_ERR "class_create failed.");
-				return PTR_ERR(cls);
-		}
-		device_create(cls, NULL, dev, NULL, "gaming%d",MINOR(dev));
+	cls = class_create(THIS_MODULE,"gaming");
+	if(IS_ERR(cls)) {
+		printk(KERN_ERR "class_create failed.");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "gaming%d",MINOR(dev));
 
-		return 0;
+	return 0;
 }
 
 static void __exit cleanup_mod(void) {
-		cdev_del(&cdv);
-		device_destroy(cls, dev);
-		class_destroy(cls);
-		unregister_chrdev_region(dev, 1);
-		printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
-		iounmap(gpio_base);
+	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+	iounmap(gpio_base);
 }
 
 module_init(init_mod);
